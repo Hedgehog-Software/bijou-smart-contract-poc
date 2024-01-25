c1f299a9495b9602addaeb088d7a3e8984a8f7e2
“elfacu0” <mahat.mejor@gmail.com>
2024-01-25 07:23:26 -0600

    Separate user functions into new file

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ mod storage_types;
 mod test;
 mod token_data;
 mod types;
+mod user;
 
 use core::cmp::min;
 
@@ -23,63 +24,12 @@ use token_data::{
 use types::{
     asset::Asset, error::Error, price_data::PriceData, state::State, token::Token, user::User,
 };
+use user::{
+    get_collateral, get_deposited_amount, get_deposited_token, get_returned_amount, get_swapped_amount, get_user_balance, get_user_deposit, get_withdrawn_collateral, has_not_repaid, is_liquidated, put_collateral, put_deposited_amount, put_deposited_token, put_returned_amount, put_swapped_amount, put_withdrawn_amount, put_withdrawn_collateral
+};
 
 fn get_admin(e: &Env) -> Option<Address> {
     e.storage().persistent().get(&DataKey::Admin)
-}
-
-fn get_and_add(e: &Env, key: DataKey, amount: i128) {
-    let mut count: i128 = e.storage().persistent().get(&key).unwrap_or_default();
-    count += amount;
-    e.storage().persistent().set(&key, &count);
-}
-
-fn get_deposited_token(e: &Env, to: &Address) -> Option<Address> {
-    let key = DataKey::DepositedToken(to.clone());
-    e.storage().persistent().get(&key)
-}
-
-fn get_deposited_amount(e: &Env, to: &Address) -> i128 {
-    let key = DataKey::DepositedAmount(to.clone());
-    e.storage().persistent().get(&key).unwrap_or_default()
-}
-
-fn get_collateral(e: &Env, to: &Address) -> i128 {
-    let key = DataKey::Collateral(to.clone());
-    e.storage().persistent().get(&key).unwrap_or_default()
-}
-
-fn get_withdrawn_collateral(e: &Env, to: &Address) -> i128 {
-    let key = DataKey::WithdrawnCollateralAmount(to.clone());
-    e.storage().persistent().get(&key).unwrap_or_default()
-}
-
-fn get_returned_amount(e: &Env, to: &Address) -> i128 {
-    e.storage()
-        .persistent()
-        .get(&DataKey::ReturnedAmount(to.clone()))
-        .unwrap_or_default()
-}
-
-fn get_swapped_amount(e: &Env, to: &Address) -> i128 {
-    e.storage()
-        .persistent()
-        .get(&DataKey::SwappedAmount(to.clone()))
-        .unwrap_or_default()
-}
-
-fn get_withdrawn_amount(e: &Env, to: &Address) -> i128 {
-    e.storage()
-        .persistent()
-        .get(&DataKey::WithdrawnAmount(to.clone()))
-        .unwrap_or_default()
-}
-
-fn get_is_liquidated(e: &Env, to: &Address) -> bool {
-    e.storage()
-        .persistent()
-        .get(&DataKey::IsLiquidated(to.clone()))
-        .unwrap_or(false)
 }
 
 fn get_spot_rate(e: &Env) -> i128 {
@@ -108,48 +58,12 @@ fn put_admin(e: &Env, address: Address) {
     e.storage().persistent().set(&DataKey::Admin, &address);
 }
 
-fn put_deposited_token(e: &Env, to: &Address, token: &Address) {
-    e.storage()
-        .persistent()
-        .set(&DataKey::DepositedToken(to.clone()), &token);
-}
-
 fn put_forward_rate(e: &Env, rate: i128) {
     e.storage().persistent().set(&DataKey::ForwardRate, &rate);
 }
 
 fn put_spot_rate(e: &Env, amount: i128) {
     e.storage().persistent().set(&DataKey::SpotRate, &amount);
-}
-
-fn put_deposited_amount(e: &Env, to: &Address, amount: i128) {
-    let key = DataKey::DepositedAmount(to.clone());
-    get_and_add(e, key, amount);
-}
-
-fn put_swapped_amount(e: &Env, to: &Address, amount: i128) {
-    let key = DataKey::SwappedAmount(to.clone());
-    get_and_add(e, key, amount);
-}
-
-fn put_collateral(e: &Env, to: &Address, amount: i128) {
-    let key = DataKey::Collateral(to.clone());
-    get_and_add(e, key, amount);
-}
-
-fn put_withdrawn_collateral(e: &Env, to: &Address, amount: i128) {
-    let key = DataKey::WithdrawnCollateralAmount(to.clone());
-    get_and_add(e, key, amount);
-}
-
-fn put_withdrawn_amount(e: &Env, to: &Address, amount: i128) {
-    let key = DataKey::WithdrawnAmount(to.clone());
-    get_and_add(e, key, amount);
-}
-
-fn put_returned_amount(e: &Env, to: &Address, amount: i128) {
-    let key = DataKey::ReturnedAmount(to.clone());
-    get_and_add(e, key, amount);
 }
 
 fn put_init_time(e: &Env) {
@@ -203,12 +117,6 @@ fn max_time_reached(e: &Env) -> bool {
     let time_to_mature = get_time_to_mature(&e);
     let time_limit: u64 = init_time + TIME_TO_EXEC + time_to_mature + TIME_TO_REPAY;
     ledger_timestamp >= time_limit
-}
-
-fn has_not_repaid(e: &Env, to: &Address) -> bool {
-    let swapped_amount = get_swapped_amount(&e, &to);
-    let returned_amount = get_returned_amount(&e, &to);
-    swapped_amount >= returned_amount
 }
 
 fn liquidate_user(e: &Env, to: &Address, from: &Address, spot_price: i128) -> i128 {
@@ -284,26 +192,6 @@ fn get_oracle_spot_price(e: &Env) -> PriceData {
 }
 
 // User
-
-fn get_user_balance(e: &Env, to: &Address) -> User {
-    let deposited_token = get_deposited_token(&e, &to).unwrap();
-    User {
-        deposited_token,
-        deposited_amount: get_deposited_amount(&e, &to),
-        swapped_amount: get_swapped_amount(&e, &to),
-        returned_amount: get_returned_amount(&e, &to),
-        withdrawn_amount: get_withdrawn_amount(&e, &to),
-        refunded_amount: 0,
-        collateral: get_collateral(&e, &to),
-        is_liquidated: get_is_liquidated(&e, &to),
-    }
-}
-
-fn get_user_deposit(e: &Env, to: &Address) -> (i128, i128) {
-    let deposited_amount = get_deposited_amount(&e, &to);
-    let collateral = get_collateral(&e, &to);
-    (deposited_amount, collateral)
-}
 
 fn get_user_amount_to_repay(e: &Env, to: &Address) -> i128 {
     let forward_rate = get_forward_rate(&e);
@@ -732,7 +620,7 @@ impl SwapTrait for Swap {
     fn reclaim_col(e: Env, to: Address) -> Result<i128, Error> {
         to.require_auth();
 
-        if get_is_liquidated(&e, &to) {
+        if is_liquidated(&e, &to) {
             return Err(Error::LiquidatedUser);
         }
 
@@ -793,7 +681,7 @@ impl SwapTrait for Swap {
             return Err(Error::InvalidToken);
         }
 
-        if get_is_liquidated(&e, &to) {
+        if is_liquidated(&e, &to) {
             return Err(Error::LiquidatedUser);
         }
 
