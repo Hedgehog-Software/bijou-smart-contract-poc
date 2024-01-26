e1322843dda7208864551dd107fe8b418dc1aab6
“elfacu0” <mahat.mejor@gmail.com>
2024-01-26 10:17:28 -0600

    Add functions to get used positions

--- src/position.rs
+++ src/position.rs
@@ -4,7 +4,21 @@ use types::position::Position;
 
 use crate::storage_types;
 use crate::token_data::get_token_a_address;
-use crate::types;
+use crate::types::{self};
+
+pub fn get_used_positions_a(e: &Env) -> Vec<Position> {
+    e.storage()
+        .persistent()
+        .get(&DataKey::UsedPositionsA)
+        .unwrap_or(Vec::new(&e))
+}
+
+pub fn get_used_positions_b(e: &Env) -> Vec<Position> {
+    e.storage()
+        .persistent()
+        .get(&DataKey::UsedPositionsB)
+        .unwrap_or(Vec::new(&e))
+}
 
 fn get_used_positions(e: &Env, token: &Address) -> Vec<Position> {
     let token_a_address = get_token_a_address(&e);
@@ -37,7 +51,7 @@ pub fn create_position(e: &Env, to: &Address, token: &Address) -> u32 {
 
 pub fn set_position_valid(e: &Env, position_index: u32, token: &Address) {
     let mut used_position = get_used_positions(&e, &token);
-    let mut position = used_position.get(0).unwrap();
+    let mut position = used_position.get(position_index).unwrap();
     position.is_valid = true;
     used_position.set(position_index, position);
     put_used_positions(&e, &token, &used_position);
