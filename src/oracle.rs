cca6912663fd075a2a44ae48df240f6449931eb3
“elfacu0” <mahat.mejor@gmail.com>
2024-02-09 06:15:10 -0600

    Extract oracle function into a new file

---
+++ src/oracle.rs
@@ -0,0 +1,17 @@
+use crate::constants::ORACLE_ADDRESS;
+use crate::token_data::{get_token_a, get_token_b};
+use soroban_sdk::{vec, Address, Env, String, Symbol};
+use types::{asset::Asset, price_data::PriceData};
+
+use crate::types;
+pub fn get_oracle_spot_price(e: &Env) -> PriceData {
+    let oracle_address: String = String::from_str(&e, ORACLE_ADDRESS);
+    let target: Address = Address::from_string(&oracle_address);
+    let func: Symbol = Symbol::new(&e, "x_last_price");
+    let base_token = get_token_a(&e).name;
+    let base_asset = Asset::Other(base_token);
+    let quote_token = get_token_b(&e).name;
+    let quote_asset = Asset::Other(quote_token);
+    let args = vec![&e, base_asset, quote_asset].to_vals();
+    e.invoke_contract::<PriceData>(&target, &func, args)
+}
