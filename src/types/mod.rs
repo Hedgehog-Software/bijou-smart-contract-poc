ca42c9bea0114aed78ed55e2bda1a231417b8f75
“elfacu0” <mahat.mejor@gmail.com>
2024-02-21 06:56:45 -0600

    Format code

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,11 +1,11 @@
 pub mod asset;
 pub mod asset_type;
 pub mod error;
+pub mod position;
+pub mod position_data;
 pub mod price_data;
 pub mod state;
+pub mod storage;
 pub mod token;
 pub mod user;
-pub mod position_data;
-pub mod position;
-pub mod storage;
 pub mod user_liq_data;
